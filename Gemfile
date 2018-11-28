f6603b08905bd43a31de580c3df57f1d624bf80b
HeejaeJoo <ymount02@gmail.com>
2018-11-28 19:57:37 +0900

    Add logger.

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'grape'
 gem 'puma'
 gem 'mongo', '>= 2.5.1'
+gem "grape_logging", "~> 1.8"
--- api/endpoint.rb
+++ api/endpoint.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class Endpoint < Grape::API
+      logger.formatter = GrapeLogging::Formatters::Rails.new
+      use GrapeLogging::Middleware::RequestLogger, { logger: logger }
+
       mount History::Endpoint => '/v1/history'
     end
   end
