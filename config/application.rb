9dca1696b68185d12aa5fd4407c35f299397f0b4
HeejaeJoo <ymount02@gmail.com>
2018-11-29 14:38:41 +0900

    Modify comment.

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,7 @@ require 'boot'
 
 Bundler.require(:default, ENV['RACK_ENV'])
 
-# To avoid using pattern like **/*.rb,
-# include all directories containing rb files explicitly.
+# Include all directories containing rb files explicitly to ensure module loading order.
 ['api/helper', 'api/v1/history/eosio_history_api', 'api/v1/history', 'api'].each do |path|
   Dir[File.expand_path("../../#{path}/*.rb", __FILE__)].each do |f|
     require f
