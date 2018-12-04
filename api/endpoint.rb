9babb1d126050c77beda92980795f12f8f29deac
HeejaeJoo <ymount02@gmail.com>
2018-12-04 11:56:57 +0900

    Add health_check api.

--- api/endpoint.rb
+++ api/endpoint.rb
@@ -4,6 +4,10 @@ module Api
       logger.formatter = GrapeLogging::Formatters::Rails.new
       use GrapeLogging::Middleware::RequestLogger, { logger: logger }
 
+      get '/health_check' do
+        status 200
+      end
+
       mount History::Endpoint => '/v1/history'
     end
   end
